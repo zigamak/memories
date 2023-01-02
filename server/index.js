ef28362f4f3929af170c7ce57a4a91af5703de53
zigamak <zigamakinde@gmail.com>
2023-01-02 18:45:35 +0100

    Import routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use 
 
 app.use(bodyParser.json({limit:"30mb ", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb ", extended:true}));
@@ -14,3 +17,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT, ()=> console.log('Server running on port : ${PORT}')))
     .catch((error)=>console.error(error.message));
+
+mongoose.set('useFindAndModify',false); 
