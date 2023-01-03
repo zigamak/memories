44673cbd0ff0c0a1e418eba6b3308c23d20ada6b
zigamak <zigamakinde@gmail.com>
2023-01-03 20:39:03 +0100

    Added schema for posts

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,3 +1,21 @@
 import mongoose from "mongoose";
 
-const PostSchema
+const PostSchema= mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags :[String],
+    selectedFile:String,
+    likecount: {
+        type:Number,
+        default:0
+    },
+    createdAt:{
+        type:Date,
+        default: new Date()
+    },
+});
+
+const PostMessage = mongoose.model('PostMessage', PostSchema);
+
+export deafult PostMessage;
