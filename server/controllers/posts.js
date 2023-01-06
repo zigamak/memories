02366de7e4fc0ae738ed1366e52f7d2c5af6b0b4
zigamak <zigamakinde@gmail.com>
2023-01-06 23:01:44 +0100

    Updated create post function

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,6 +19,6 @@ export const createPost = async (req, res) =>{
             
             res.status(201).json(newPost);
         }catch(error){
-            res.status(409).json(message: error.message)
+            res.status(409).json({message: error.message})
         }
 }
