4780c7a70752db350ed79762ffa36c8a44147485
zigamak <zigamakinde@gmail.com>
2023-01-06 23:19:28 +0100

    Added post directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <Container maxwidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typograhy variant="h2" align="center">
+            Memories
+          </Typograhy>
+        </AppBar>
+      </Container>
+    </div>  
+    );
+  }
 
 export default App;
