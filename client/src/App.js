53a034c4c97488516e56c7c55626bbbba8d74b05
zigamak <zigamakinde@gmail.com>
2023-01-07 20:57:00 +0100

    Added logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.png';
 function App() {
   return (
     <div>
       <Container maxwidth="lg">
         <AppBar position="static" color="inherit">
           <Typograhy variant="h2" align="center">
-            Memories
+              <img src={memories} alt= "memories" height='60' />
           </Typograhy>
+          <Grow in> 
+            <Container>
+              <Grid container justify="space-between" alignItems="stretch" spacing={3}> 
+
+              </Grid>
+            </Container>
+          </Grow>
         </AppBar>
       </Container>
     </div>  
     );
-  }
+  } 
 
 export default App;
