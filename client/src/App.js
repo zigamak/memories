01c78e7700a63a405def860da12ea82ed93f4aae
zigamak <zigamakinde@gmail.com>
2023-01-07 21:35:41 +0100

    Added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from  './components/Form/Form';
 import memories from './images/memories.png';
 function App() {
   return (
     <div>
       <Container maxwidth="lg">
         <AppBar position="static" color="inherit">
-          <Typograhy variant="h2" align="center">
+          <Typography variant="h2" align="center">
               <img src={memories} alt= "memories" height='60' />
-          </Typograhy>
+          </Typography>
           <Grow in> 
             <Container>
-              <Grid container justify="space-between" alignItems="stretch" spacing={3}> 
-
+              <Grid container ="space-between" alignItems="stretch" spacing={3}> 
+                <Grid item xs={12} sm={7}>
+                  <Posts/>                  
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form/>
+                </Grid>
               </Grid>
             </Container>
           </Grow>
