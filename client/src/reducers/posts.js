cfe028eb8551a27aa2bd6f3142991978bcf6cc0a
zigamak <zigamakinde@gmail.com>
2023-01-11 20:48:15 +0100

    Implemented reducers for posts

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,12 @@
+const reducer =(state, action) =>{
+    switch (action.type) {
+        case 'FETCH_ALL'  :
+            return state;
+        case 'CREATE' :
+            return state;
+            break;
+    
+        default:
+            break;
+    }
+}
