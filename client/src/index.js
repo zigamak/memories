4dc3994e7950ce9ebb202ed59b8e7f16766048ff
zigamak <zigamakinde@gmail.com>
2023-01-12 19:56:58 +0100

    Added provider to the index file

--- client/src/index.js
+++ client/src/index.js
@@ -8,4 +8,8 @@ import reducers  from './reducers';
 import App from './App';
 
 const store = configureStore(reducers, compose(applyMiddleware(thunk)))
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store ={store}>
+    <App/>
+</Provider>,
+document.getElementById('root'));
