cc45b25cfc39cc5216c037f9240ed94560c64542
zigamak <zigamakinde@gmail.com>
2023-01-13 21:35:32 +0100

    Returned the payload in the reducers

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 const reducer =(state, action) =>{
     switch (action.type) {
         case 'FETCH_ALL'  :
-            return state;
+            return action.payload;
         case 'CREATE' :
             return state;
             break;
