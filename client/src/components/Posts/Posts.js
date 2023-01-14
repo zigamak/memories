e3ef37beb84cccc428a4a466949b9830f6762e89
zigamak <zigamakinde@gmail.com>
2023-01-14 20:22:08 +0100

    Added selectors to the component post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Post from './Post/Post';
+import {useSelector} from 'react-redux';
 import useStyles from './styles';
  
 const Posts = ()=>{
     const classes = useStyles();
+    const post = useSelector((state)=>state.posts);
+
+    console.log(posts);
     return (
         <>
             <h1>POSTS</h1>
