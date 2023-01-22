c0622e5bb6141ec890c1668dfa234d2d0d6b607a
zigamak <zigamakinde@gmail.com>
2023-01-22 21:01:24 +0100

    Updated styles for posts

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {getPosts} from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from  './components/Form/Form';
 import memories from './images/memories.png';
+// Imported styles 
 import useStyles from './styles';
 function App() {
     const classes = useStyles();
