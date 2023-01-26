a773e59ba4d75db969f518dae4eaa18be0fee636
zigamak <zigamakinde@gmail.com>
2023-01-26 20:30:46 +0100

    Updated AppBAR

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ function App() {
     <div>
       {/*Updated Container*/}
       <Container maxwidth="lg">
+        {/*Updated AppBAR*/}
         <AppBar className={classes.appBar} position="static" color="inherit">
           <Typography className={classes.heading} variant="h2" align="center">
               <img className={classes.img} src={memories} alt= "memories" height='60' />
