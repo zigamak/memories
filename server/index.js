aa1980609c2d5bd9525d06cc06f80b9c2e85e9df
zigamak <zigamakinde@gmail.com>
2023-02-10 21:50:15 +0100

    Added find and modify for mongoose

--- server/index.js
+++ server/index.js
@@ -17,5 +17,5 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT, ()=> console.log('Server running on port : ${PORT}')))
     .catch((error)=>console.error(error.message));
-
+// Added find and modify 
 mongoose.set('useFindAndModify',false); 
