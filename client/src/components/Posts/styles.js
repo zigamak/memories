713aea02ddeb48822df1f684e0a7cc8ef174410e
zigamak <zigamakinde@gmail.com>
2023-02-16 20:57:22 +0100

    Updated post styles

--- client/src/components/Posts/styles.js
+++ client/src/components/Posts/styles.js
@@ -1,18 +1,14 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
-  appBar: {
-    borderRadius: 15,
-    margin: '30px 0',
+export default makeStyles((theme) => ({
+  mainContainer: {
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
     alignItems: 'center',
   },
-  heading: {
-    color: 'rgba(0,183,255, 1)',
+  smMargin: {
+    margin: theme.spacing(1),
   },
-  image: {
-    marginLeft: '15px',
+  actionDiv: {
+    textAlign: 'center',
   },
 }));
