c2e348ef36f0e750e2ee8edc530440b8535cab31
zigamak <zigamakinde@gmail.com>
2023-02-16 20:56:12 +0100

    Added Circular progress to post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,25 @@
-import express from 'express';
-//Imported getPosts, getPost, createPost, updatePost
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
-const router = express.Router();
+import Post from './Post/Post';
+import useStyles from './styles';
 
-router.get('/', getPosts);
-router.post('/', createPost);
-router.get('/:id', getPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
-export default router;
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
+
+export default Posts;
