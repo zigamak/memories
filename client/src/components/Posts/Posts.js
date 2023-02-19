5907080517da735dafda9d13b9ff446967864fa4
zigamak <zigamakinde@gmail.com>
2023-02-19 21:44:42 +0100

    Updated posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,7 +4,7 @@ import Post from './Post/Post';
 import { Grid,  CircularProgress } from '@material-ui/core';
 import {useSelector} from 'react-redux';
 import useStyles from './styles';
- 
+// 
 const Posts = ()=>{
     const classes = useStyles()
     
