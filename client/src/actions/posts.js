cef26f6d8782e07d4c3947060da1215132c6b5d8
zigamak <zigamakinde@gmail.com>
2023-02-24 21:10:51 +0100

    Added likePost function

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -33,7 +33,7 @@ export const getPosts = () => async (dispatch) => {
      console.log(error.message);
    }
  };
- 
+ //
  export const likePost = (id) => async (dispatch) => {
    try {
      const { data } = await api.likePost(id);
