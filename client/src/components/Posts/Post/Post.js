0f3674d3dbb795d8bd3b9b5f16ddac0b99380905
zigamak <zigamakinde@gmail.com>
2023-02-26 20:47:52 +0100

    Added Card design for post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -14,6 +14,7 @@ const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
 
   return (
+    //Added Card design for post
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
       <div className={classes.overlay}>
